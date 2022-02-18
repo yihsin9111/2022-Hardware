13529f6e9d0e5e75ffe10af81170d40cb2d657d7
Ken <b09901028@ntu.edu.tw>
2022-02-18 10:37:09 +0800

    merge

--- pca9956/Examples/ALL_SAME_COLOR.cpp
+++ pca9956/Examples/ALL_SAME_COLOR.cpp
@@ -20,41 +20,41 @@ int main(int argc, char *argv[]){
 
     while(cin >> color){
         switch (color){
-	    case '0':
-		LED reset;
-		reset.IREF_R = 134;
-		reset.IREF_G = 134;
-		reset.IREF_B = 134;
-		reset.PWM_R = 0;
-		reset.PWM_G = 0;
-		reset.PWM_B = 0;
+	        case '0':
+                LED reset;
+                reset.IREF_R = 134;
+                reset.IREF_G = 134;
+                reset.IREF_B = 134;
+                reset.PWM_R = 0;
+                reset.PWM_G = 0;
+                reset.PWM_B = 0;
             case 'R':
                 LED red;
                 red.IREF_R = 134;
-		red.IREF_G = 134;
-		red.IREF_B = 134;
-		red.PWM_R = 255;
+                red.IREF_G = 134;
+                red.IREF_B = 134;
+                red.PWM_R = 255;
                 red.PWM_G = 0;
-		red.PWM_B = 0;
+		        red.PWM_B = 0;
                 Set_All_Same_Color(pca9956_led, red);
                 break;
             case 'G':
                 LED green;
-		green.IREF_R = 134;
+		        green.IREF_R = 134;
                 green.IREF_G = 134;
-		green.IREF_B = 134;
-		green.PWM_R = 0;
+		        green.IREF_B = 134;
+		        green.PWM_R = 0;
                 green.PWM_G = 255;
-		green.PWM_B = 0;
+		        green.PWM_B = 0;
                 Set_All_Same_Color(pca9956_led, green);
                 break;
             case 'B':
                 LED blue;
-		blue.IREF_R = 134;
-		blue.IREF_G = 134;
+                blue.IREF_R = 134;
+                blue.IREF_G = 134;
                 blue.IREF_B = 134;
-		blue.PWM_R = 0;
-		blue.PWM_G = 0;
+                blue.PWM_R = 0;
+                blue.PWM_G = 0;
                 blue.PWM_B = 255;
                 Set_All_Same_Color(pca9956_led, blue);
                 break;
@@ -64,14 +64,14 @@ int main(int argc, char *argv[]){
                 yellow.PWM_R = 255;
                 yellow.IREF_G = 134;
                 yellow.PWM_G = 255;
-		yellow.IREF_B = 134;
-		yellow.PWM_B = 0;
+                yellow.IREF_B = 134;
+                yellow.PWM_B = 0;
                 Set_All_Same_Color(pca9956_led, yellow);
                 break;
             case 'C':
                 LED cyan;
-		cyan.IREF_R = 134;
-		cyan.PWM_R = 0;
+                cyan.IREF_R = 134;
+                cyan.PWM_R = 0;
                 cyan.IREF_B = 134;
                 cyan.PWM_B = 255;
                 cyan.IREF_G = 134;
@@ -82,8 +82,8 @@ int main(int argc, char *argv[]){
                 LED magenta;
                 magenta.IREF_B = 134;
                 magenta.PWM_B = 255;
-		magenta.IREF_G = 134;
-		magenta.PWM_G = 0;
+                magenta.IREF_G = 134;
+                magenta.PWM_G = 0;
                 magenta.IREF_R = 134;
                 magenta.PWM_R = 255;
                 Set_All_Same_Color(pca9956_led, magenta);
