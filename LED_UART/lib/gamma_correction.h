d54e0e6af2a366f2b0279860402afaccfbd0cb75
黃柏穎 <b09901089@g.ntu.edu.tw>
2022-02-23 00:39:17 +0800

    add rgba_to_rgb

--- LED_UART/lib/gamma_correction.h
+++ LED_UART/lib/gamma_correction.h
@@ -1,14 +1,15 @@
 #include <math.h>
 #include <iostream>
 
-class Color_regulator{
-  public:
-    Color_regulator(){};
-    Color_regulator(float );
-    int gamma_correction(float c);
-    void rgba_to_rgb(float &r, float &g, float &b, float &a);
-    void set_gamma(float &);
-  private:
-    float gamma;
+class Color_regulator
+{
+public:
+  Color_regulator(){};
+  Color_regulator(float);
+  int gamma_correction(float c);
+  void set_gamma(float &);
 
+private:
+  float gamma;
 };
+float *rgba_to_rgb(float &r, float &g, float &b, float &a, int max_brightness);
