ca5b8269aca411cb82853b28806219200912b181
Ken <b09901028@ntu.edu.tw>
2022-02-24 17:58:51 +0800

    color picker.ino rv

--- colorpicker/colorpicker.ino
+++ colorpicker/colorpicker.ino
@@ -2,61 +2,72 @@
 #include <FastLED.h>
 #include "gamma_correction.h"
 #include "rgba_to_rgb.h"
-#define DATA_PIN 11
+
+#define LED_DATA_PIN 11
+
+#define LED_ALPHA A1
+#define LED_GREEN A2
+#define LED_RED   A4
+#define LED_BLUE  A5
+
+#define OF_ALPHA  A6
+#define OF_RED    A7
+#define OF_GREEN  A8
+#define OF_BLUE   A9
+
 const byte interruptPin = 2;
 
 int num_leds = 30;
 // the grb that the user input
-int sigR = 0, sigG = 0, sigB = 0;
-int sigA = 0;
-int iRefg = 0, iRefr = 0, iRefb = 0;
+int ledSigR = 0, ledSigG = 0, ledSigB = 0, ledSigA = 0;
+int ofSigR = 0, ofSigG = 0, ofSigB = 0, ofSigA = 0;
 // false means color picker off
 bool mode = true;
 CRGB leds[35];
 
 void change() {
-  mode = false;
+  mode = !mode;
 }
 
 void LCD_Show(int sigR, int sigG, int sigB, int sigA) {
   lcd.setCursor(0,0);
-    lcd.print("R : ");
-    lcd.setCursor(4,0);
-    lcd.print(sigR, DEC);
-    if(sigR<10){
-      lcd.setCursor(5,0);
-      lcd.print("  ");
-    }
-    else if(sigR<100){
-      lcd.setCursor(6,0);
-      lcd.print(" ");
-    }
-    lcd.setCursor(7,0);
-    lcd.print(", G : ");
-    lcd.setCursor(13,0);
-    lcd.print(sigG, DEC);
-    if(sigG<10){
-      lcd.setCursor(14,0);
-      lcd.print("  ");
-    }
-    else if(sigG<100){
-      lcd.setCursor(15,0);
-      lcd.print(" ");
-    }
-    lcd.setCursor(0,1);
-    lcd.print("B : ");
-    lcd.setCursor(4,1);
-    lcd.print(sigB, DEC);
-    if(sigB<10){
-      lcd.setCursor(5,1);
-      lcd.print("  ");
-    }
-    else if(sigB<100){
-      lcd.setCursor(6,1);
-      lcd.print(" ");
-    }
+  lcd.print("R : ");
+  lcd.setCursor(4,0);
+  lcd.print(sigR, DEC);
+  if(sigR<10){
+    lcd.setCursor(5,0);
+    lcd.print("  ");
+  }
+  else if(sigR<100){
+    lcd.setCursor(6,0);
+    lcd.print(" ");
+  }
+  lcd.setCursor(7,0);
+  lcd.print(", G : ");
+  lcd.setCursor(13,0);
+  lcd.print(sigG, DEC);
+  if(sigG<10){
+    lcd.setCursor(14,0);
+    lcd.print("  ");
+  }
+  else if(sigG<100){
+    lcd.setCursor(15,0);
+    lcd.print(" ");
+  }
+  lcd.setCursor(0,1);
+  lcd.print("B : ");
+  lcd.setCursor(4,1);
+  lcd.print(sigB, DEC);
+  if(sigB<10){
+    lcd.setCursor(5,1);
+    lcd.print("  ");
+  }
+  else if(sigB<100){
+    lcd.setCursor(6,1);
+    lcd.print(" ");
   }
 }
+
 
 void setup() {
   LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x26, 16, 2); // Change to (0x27,20,4) for 20x4 LCD.
@@ -70,7 +81,7 @@ void setup() {
   delay(1000);
   lcd.clear();
   // initialize leds
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, num_leds);
+  FastLED.addLeds<NEOPIXEL, LED_DATA_PIN>(leds, num_leds);
   for (int i = 0; i < num_leds; i++)
   {
     leds[i] = CRGB::Black;
@@ -79,10 +90,13 @@ void setup() {
   // initialize serial
   Serial.begin(9600);
   // initialize knobs
-  pinMode(A1,INPUT); // a
-  pinMode(A2,INPUT); // g
-  pinMode(A4,INPUT); // r
-  pinMode(A5,INPUT); // b
+  pinMode(LED_ALPHA,INPUT); // led a
+  pinMode(LED_GREEN,INPUT); // led g
+  pinMode(LED_RED,INPUT); // led r
+  pinMode(LED_BLUE,INPUT); // led b
+  
+  pinMode(OF_ALPHA, INPUT); //
+  
   // Interrupt--Change Mode
   attachInterrupt(digitalPinToInterrupt(interruptPin), change, CHANGE);
 }
@@ -93,19 +107,18 @@ void loop()
   // 調色模式on
   if (mode)
   {
-    float R_out = 0f, G_out = 0f, B_out = 0f;
     float *color;
-    sigA = analogRead(A1) / 4;
-    sigG = analogRead(A2) / 4;
-    sigR = analogRead(A4) / 4;
-    sigB = analogRead(A5) / 4;
-    total = sigG + sigR + sigB;
+    ledSigA = analogRead(A1) / 4;
+    ledSigG = analogRead(A2) / 4;
+    ledSigR = analogRead(A4) / 4;
+    ledSigB = analogRead(A5) / 4;
+    total = ledSigG + ledSigR + ledSigB;
 
-    sigG = sigG * 255 / total;
-    sigR = sigR * 255 / total;
-    sigB = sigB * 255 / total;
+    ledSigG = ledSigG * 255 / total;
+    ledSigR = ledSigR * 255 / total;
+    ledSigB = ledSigB * 255 / total;
     // color[0]:g color[1]:r color[2]:b
-    color = rgba_to_rgb(sigG, sigR, sigB, sigA, 200);
+    color = rgba_to_rgb(ledSigG, ledSigR, ledSigB, ledSigA, 200);
 
     for (int i = 0; i < NUM_LEDS; i++)
     {
@@ -116,15 +129,14 @@ void loop()
   // 調色模式off
   else {
     // read RGB and light them 
-    sigR = analogRead(A2)/4;
-    sigG = analogRead(A4)/4;
-    sigB = analogRead(A5)/4;
+    ledSigR = analogRead(A2)/4;
+    ledSigG = analogRead(A4)/4;
+    ledSigB = analogRead(A5)/4;
     for(int i=0;i<NUM_LEDS;i++){
-      leds[i] = CRGB(sigR, sigG, sigB);
+      leds[i] = CRGB(ledSigR, ledSigG, ledSigB);
       FastLED.show();
     }
     // show 
-    
-
+  }
   
 }
