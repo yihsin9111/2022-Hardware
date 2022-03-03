4fdebeffbd0f4fd590f7921bc9ea8c896f880328
AliceDu2002 <alicedu0211@gmail.com>
2022-03-03 15:02:57 +0800

    colorpicker lcd finished

--- colorpicker/colorpicker.ino
+++ colorpicker/colorpicker.ino
@@ -21,8 +21,8 @@
 const byte interruptPin = 2;
 
 // the grb that the user input
-int ledSigR = 0, ledSigG = 0, ledSigB = 0, ledSigA = 0;
-int ofSigR = 0, ofSigG = 0, ofSigB = 0, ofSigA = 0;
+double ledSigR = 0, ledSigG = 0, ledSigB = 0, ledSigA = 0;
+double ofSigR = 0, ofSigG = 0, ofSigB = 0, ofSigA = 0;
 // false means color picker off
 bool mode = true;
 CRGB leds[35];
@@ -54,10 +54,18 @@ void Align(LiquidCrystal_I2C b, String type, int n) {
   }
 }
 
-float Round(float a) {
-  a *= 10;
-  a = round(a);
-  a /= 10;
+float Round_LED(float a) {
+  a /= 170;
+  a *= 1000;
+  int a_show = int(round(a));
+  return a_show;
+}
+
+float Round_OF(float a) {
+  a /= 255;
+  a *= 1000;
+  int a_show = int(round(a));
+  return a_show;
 }
 
 void sLCD_Show(int ledR, int ledG, int ledB, float ledA, int ofR, int ofG, int ofB, float ofA) {
@@ -67,7 +75,8 @@ void sLCD_Show(int ledR, int ledG, int ledB, float ledA, int ofR, int ofG, int o
   Align(sLCD, "G", ledG);
   Align(sLCD, "B", ledB);
   sLCD.setCursor(4, 1);
-  ledA = Round(ledA);
+  int ledA_show = Round_LED(ledA);
+  ledA = ledA_show * 0.001;
   sLCD.print("A");
   sLCD.print(ledA, DEC);
   
@@ -76,17 +85,20 @@ void sLCD_Show(int ledR, int ledG, int ledB, float ledA, int ofR, int ofG, int o
   Align(sLCD, "R", ofR);
   Align(sLCD, "G", ofG);
   Align(sLCD, "B", ofB);
-  sLCD.setCursor(4, 1);
-  ofA = Round(ofA);
+  sLCD.setCursor(4, 3);
+  int ofA_show = Round_OF(ofA);
+  ofA = ofA_show * 0.001;
   sLCD.print("A");
-  sLCD.print(ofA);
+  sLCD.print(ofA, DEC);
 }
 
 void hLCD_Show(int IR, int IG, int IB, int PR, int PG, int PB, int ledR, int ledG, int ledB) {
   hLCD.setCursor(0, 0);
   hLCD.print("LED ");
   Align(hLCD, "R", ledR);
+  hLCD.setCursor(10, 0);
   Align(hLCD, "G", ledG);
+  hLCD.setCursor(16, 0);
   Align(hLCD, "B", ledB);
   hLCD.setCursor(0, 1); 
   hLCD.print("OF ");   
@@ -115,7 +127,7 @@ void setup() {
   hLCD.print("Mode: Color Picking");
   hLCD.setCursor(2, 1); //Set the cursor on the third column and the second row (counting starts at 0!).
   hLCD.print("for hardware team");  
-  delay(1000);
+  delay(3000);
   hLCD.clear();
   // initialize leds
   FastLED.addLeds<NEOPIXEL, LED_DATA_PIN>(leds, NUM_LEDS);
@@ -141,6 +153,8 @@ void setup() {
   Wire.begin();
   // Interrupt--Change Mode
   attachInterrupt(digitalPinToInterrupt(interruptPin), change, CHANGE);
+
+  Serial.begin(9600);
 }
 
 void loop()
@@ -160,7 +174,12 @@ void loop()
     ledSigG = ledSigG * 255 / ledTotal;
     ledSigR = ledSigR * 255 / ledTotal;
     ledSigB = ledSigB * 255 / ledTotal;
-    // color[0]:g color[1]:r color[2]:b
+    // color[0]:r color[1]:g color[2]:b
+//    Serial.print(int(ledSigR));
+//    Serial.print(' ');
+//    Serial.print(int(ledSigG));
+//    Serial.print(' ');
+//    Serial.println(int(ledSigB));
     LEDcolor = LEDrgba_to_rgb(ledSigG, ledSigR, ledSigB, ledSigA, 200);
 
     for (int i = 0; i < NUM_LEDS; i++)
@@ -188,15 +207,15 @@ void loop()
     //Set IREF
     Wire.beginTransmission(ADDRESS); // transmit to device
     Wire.write(0x22);        // sends five bytes
-    Wire.write(ofSigR);              // sends one byte
+    Wire.write(int(ofSigR));              // sends one byte
     Wire.endTransmission();    // stop transmitting
     Wire.beginTransmission(ADDRESS); // transmit to device
     Wire.write(0x23);        // sends five bytes
-    Wire.write(ofSigG);              // sends one byte
+    Wire.write(int(ofSigG));              // sends one byte
     Wire.endTransmission();    // stop transmitting
     Wire.beginTransmission(ADDRESS); // transmit to device
     Wire.write(0x24);        // sends five bytes
-    Wire.write(ofSigB);              // sends one byte
+    Wire.write(int(ofSigB));              // sends one byte
     Wire.endTransmission();    // stop transmitting
     Wire.beginTransmission(ADDRESS); // transmit to device
     Wire.write(0x0a);        // sends five bytes
@@ -210,7 +229,15 @@ void loop()
     Wire.write(0x0c);        // sends five bytes
     Wire.write(int(OFcolor[2]));              // sends one byte
     Wire.endTransmission();    // stop transmitting
-
+    // show 
+    hLCD_Show(ofSigR, ofSigG, ofSigB, int(OFcolor[0]), int(OFcolor[1]), int(OFcolor[2]), LEDcolor[0], LEDcolor[1], LEDcolor[0]);
+//    Serial.print(int(ledSigR));
+//    Serial.print(' ');
+//    Serial.print(int(ledSigG));
+//    Serial.print(' ');
+//    Serial.println(int(ledSigB));
+    sLCD_Show(int(ledSigR), int(ledSigG), int(ledSigB), ledSigA, ofSigR, ofSigG, ofSigB, ofSigA);
+    delay(50);
   }
   // 調色模式off
   else {
@@ -239,15 +266,15 @@ void loop()
     //Set IREF
     Wire.beginTransmission(ADDRESS); // transmit to device
     Wire.write(0x22);        // sends five bytes
-    Wire.write(ofSigR);              // sends one byte
+    Wire.write(int(ofSigR));              // sends one byte
     Wire.endTransmission();    // stop transmitting
     Wire.beginTransmission(ADDRESS); // transmit to device
     Wire.write(0x23);        // sends five bytes
-    Wire.write(ofSigG);              // sends one byte
+    Wire.write(int(ofSigG));              // sends one byte
     Wire.endTransmission();    // stop transmitting
     Wire.beginTransmission(ADDRESS); // transmit to device
     Wire.write(0x24);        // sends five bytes
-    Wire.write(ofSigB);              // sends one byte
+    Wire.write(int(ofSigB));              // sends one byte
     Wire.endTransmission();    // stop transmitting
     Wire.beginTransmission(ADDRESS); // transmit to device
     Wire.write(0x0a);        // sends five bytes
@@ -263,6 +290,6 @@ void loop()
     Wire.endTransmission();    // stop transmitting
 
     // show 
+    sLCD_Show(int(ledSigR), int(ledSigG), int(ledSigB), ledSigA, int(ofSigR), int(ofSigG), int(ofSigB), ofSigA);
   }
-  
 }
--- colorpicker/rgba_to_rgb.h
+++ colorpicker/rgba_to_rgb.h
@@ -14,11 +14,10 @@ float *LEDrgba_to_rgb(float r, float g, float b, float a, int max_brightness)
   color[1] = g * a * max_brightness;
   color[2] = b * a * max_brightness;
 
-  float temp = color[0] / 255;
-
-  color[0] = pow(temp, gamma_g) * 255;
-  temp = color[1] / 255;
-  color[1] = pow(temp, gamma_r) * 255;
+  float temp = color[1] / 255;
+  color[1] = pow(temp, gamma_g) * 255;
+  temp = color[0] / 255;
+  color[0] = pow(temp, gamma_r) * 255;
   temp = color[2] / 255;
   color[2] = pow(temp, gamma_b) * 255;
 
@@ -27,15 +26,17 @@ float *LEDrgba_to_rgb(float r, float g, float b, float a, int max_brightness)
 
 float *OFrgba_to_rgb(float a)
 {
+  Serial.println(a);
   float *color = new float[3];
-  a/=255;
-  color[0] = 1/3 * a ;
-  color[1] = 1/3 * a ;
-  color[2] = 1/3 * a ;
+  a/=765;
 
-  color[0] = pow(color[0], gamma_r) * 255;
-  color[1] = pow(color[1], gamma_g) * 255;
-  color[2] = pow(color[2], gamma_b) * 255;
-
+  color[0] = pow(a, gamma_r) * 255;
+  color[1] = pow(a, gamma_g) * 255;
+  color[2] = pow(a, gamma_b) * 255;
+  Serial.print(color[0]);
+  Serial.print(' ');
+  Serial.print(color[1]);
+  Serial.print(' ');
+  Serial.println(color[2]);  
   return color;
 };
