e477a4af4e5e6bb501826b950c58da8929edf99f
Kenchu123 <k88913n@gmail.com>
2022-03-08 23:39:19 +0800

    add rgba lib

---
+++ rgba_to_rgb_lib/rgba_to_rgb.h
@@ -0,0 +1,38 @@
+#include <math.h>
+#define gamma_r 1.6
+#define gamma_g 1.8
+#define gamma_b 1.66
+
+float *LEDrgba_to_rgb(float r, float g, float b, float a, int max_brightness)
+{
+  float *color = new float[3];
+  a /= 15;
+  r = r / (r + g + b);
+  g = g / (r + g + b);
+  b = b / (r + g + b);
+  color[0] = r * a * max_brightness;
+  color[1] = g * a * max_brightness;
+  color[2] = b * a * max_brightness;
+
+  float temp = color[0] / 255;
+  color[0] = pow(temp, gamma_r) * 255;
+  temp = color[1] / 255;
+  color[1] = pow(temp, gamma_g) * 255;
+
+  temp = color[2] / 255;
+  color[2] = pow(temp, gamma_b) * 255;
+
+  return color;
+};
+
+float *OFrgba_to_rgb(float a)
+{
+  Serial.println(a);
+  float *color = new float[3];
+  a /= 15;
+
+  color[0] = pow(a, gamma_r) * 255;
+  color[1] = pow(a, gamma_g) * 255;
+  color[2] = pow(a, gamma_b) * 255;
+  return color;
+};
