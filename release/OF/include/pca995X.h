c9782e3d4453053a1f2e568399e02d7a1515b225
Ken <b09901028@ntu.edu.tw>
2022-03-13 21:58:33 +0800

    pca release

--- release/OF/include/pca995X.h
+++ release/OF/include/pca995X.h
@@ -33,6 +33,7 @@
 // class for PCA9955 & PCA9956
 class PCA995X {
    public:
+    // constructor : initialize
     PCA995X() {}
     ~PCA995X() {}
     PCA995X(int Address, int pca_type, int iref0Reg, int pwm0Reg, int ledChannelNum);
@@ -41,8 +42,10 @@ class PCA995X {
     int SetIREFAI(int channel, int *IREF, int size);
     int SetPWMIREFAI(int *data);
 
+    // set every-three channels into one led-channels
     int SetRGB(int led_address, int Rduty, int Gduty, int Bduty, int Riref, int Giref, int Biref);
 
+    // read values in all pwm regs and iref regs
     void GetAll();
 
     int Getfd() { return fd; };
@@ -52,14 +55,18 @@ class PCA995X {
     int GetLedChannelNum();
 
    private:
+    // check if the channel is legally (larger than 0 and smaller than maximum)
     bool CheckChannelLegal(int channel);
-
+    
+    // Set/Get pwm value wrt specific channel
     int SetPWM(int channel, int PWM);
     int GetPWM(int channel);
 
+    // Set/Get iref value wrt specific channel
     int SetIREF(int channel, int IREF);
     int GetIREF(int channel);
 
+    // i2c write/read specific Reg
     int I2CWriteReg(int reg, int value);
     int I2CReadReg(int reg);
 
